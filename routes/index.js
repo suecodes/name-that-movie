80cb322bdcb6ba834a24fb2db8ca57230f5cb40d
suecodes <sueoedens@gmail.com>
2018-12-15 01:01:59 +1100

    Updates to create quiz feature on landing page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Show home page
 router.get('/', function (req, res, next) {
   res.render('index', {
+    title: 'Name That Movie'
+  });
+});
+
+// Show collection of movie quotes 
+router.get('/moviequotes', function (req, res, next) {
+  res.render('moviequotes', {
     title: 'Name That Movie'
   });
 });
