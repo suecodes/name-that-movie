631df74edca9c8664f8a835a66db496c7d0d755b
suecodes <sueoedens@gmail.com>
2018-12-15 21:47:30 +1100

    Fixed issue with wrong answer not appearing centered and display movie title if wright answer

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -3,13 +3,27 @@ var userInputBlock = document.getElementById("userinput");
 var answerBlock = document.getElementById("right-answer-block");
 var wrongBlock = document.getElementById("wrong-answer-block");
 var message = document.getElementById("message");
+var moviename = document.getElementById("movie-name");
 
+/** 
+    Checks if user entered the correct movie name 
+    - is not case sensitive
+
+    - TODO Refactor, possibly toggle style using conditional (ternary) operator
+*/
 function checkMovieQuote() {
     if (userMovieInput.value.toLowerCase() === "when harry met sally") {
+
+        // right answer
+        moviename.style.visibility = "visible";
         userInputBlock.style.display = "none";
         answerBlock.style.display = "block";
         wrongBlock.style.display = "none";
+
     } else {
+
+        // wrong answer
+        moviename.style.visibility = "hidden";
         userInputBlock.style.display = "none";
         answerBlock.style.display = "none";
         wrongBlock.style.display = "block";
