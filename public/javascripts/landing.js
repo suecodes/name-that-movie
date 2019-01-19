b4bc6d0e8bfc708af39b2b23dc45a0ddb7ff84cb
suecodes <sueoedens@gmail.com>
2019-01-19 23:01:12 +1100

    Clean up landing page and remove redundant code

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -9,13 +9,8 @@ var userMovieInput = document.getElementById("user-guess");
 var userInputBlock = document.getElementById("userinput");
 var answerBlock = document.getElementById("right-answer-block");
 var wrongBlock = document.getElementById("wrong-answer-block");
-var message = document.getElementById("message");
 var moviename = document.getElementById("movie-name");
-var quoteField = document.getElementById("moviequote");
 var movieanswer = document.querySelectorAll(".moviename");
-var wronganswer = document.getElementById("incorrectanswer");
-var rightanswer = document.getElementById("rightanswer");
-var commentCreatedDate = document.getElementById("createdDate");
 
 // Initialize
 init();
@@ -38,28 +33,23 @@ function setupAnswers() {
         // attach click event to each button
         movieanswer[i].addEventListener("click", function () {
 
+            // change button color so user knows it was clicked (eliminated)
+            this.style.backgroundColor = "#AAB7B8";
+
+            // capture movie name clicked and movie name answer
             let movieanswer = this.textContent.toLowerCase();
             let userinput = userMovieInput.value.toLowerCase();
 
+            // compare answer and display right/wrong outcome
             movieanswer === userinput ? displayAnswer() : displayWrongAnswer();
-
-            // Check if user clicked the correct answer
-            // if (this.textContent.toLowerCase() === userMovieInput.value.toLowerCase()) {
-            //     displayAnswer();
-            // } else {
-            //     // wrong answer
-            //     displayWrongAnswer();
-            // }
         });
 
     }
 }
 
 /** 
-    User requests answer to quote
-
-    - TODO Refactor, repeats code from above. Need to split into sep function and just call that function
-*/
+ * Correct answer display
+ */
 function displayAnswer() {
     moviename.style.color = "#138D75";
     moviename.style.visibility = "visible";
@@ -68,8 +58,10 @@ function displayAnswer() {
     wrongBlock.style.display = "none";
 }
 
-function displayWrongAnswer() {
-    this.style.backgroundColor = "#AAB7B8";
+/** 
+ * Wrong answer display
+ */
+function displayWrongAnswer(obj) {
     moviename.style.color = "#7B241C";
     moviename.style.visibility = "hidden";
     userInputBlock.style.display = "block";
@@ -78,24 +70,9 @@ function displayWrongAnswer() {
 }
 
 /** 
-    Reset, display input field for new guess
-*/
+ * Reset, allow user to guess again
+ */
 function guessAgain() {
-    //userMovieInput.value = "";
     userInputBlock.style.display = "block";
     wrongBlock.style.display = "none";
-}
-
-/** 
-    TODO Display quote as a typewriter effect (Not in use)
-*/
-var i = 0;
-var speed = 50;
-
-function typeWriter(name) {
-    if (i < name.length) {
-        document.getElementById("quoteField").innerHTML += name.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
 }
