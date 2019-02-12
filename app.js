e2c937103eadc5c077a965098fff7a3f4f7f6675
suecodes <sueoedens@gmail.com>
2019-02-12 22:32:43 +1100

    Fix sort by not dropping down

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ const movieCommentsRouter = require("./routes/comments");
 //var usersRouter = require('./routes/users');	// future functionality - user profile
 
 // Fill the database - comment out if you do not want to refresh db with data
-const fillDB = require("./filler");
-fillDB();
-
+//const fillDB = require("./filler");
+//fillDB();
+	
 const app = express();
 
 // DB setup
